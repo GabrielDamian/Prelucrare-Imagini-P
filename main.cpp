82daa64411df8142ae60f0bfd74ee0309064ac33
GabrielDamian <damiangabriel54@yahoo.com>
2021-11-19 00:37:09 -0800

    "update"

--- main.cpp
+++ main.cpp
@@ -6,19 +6,6 @@ int main() {
 	textDetector(img,rez);
 	btnDetector(img,rez);
 
-	/*
-	* CEVA
-	* altceva
-	//imshow("Inainte", img);
-	//imshow("Detectare text", rez);
-	Mat legenda = generateLegendCustom(rez.rows,280);
-	
-	//imagine finala (compusa din: rez + legenda)
-	Mat3b final(rez.rows, rez.cols+legenda.cols, Vec3b(0, 0, 0));
-	
-	rez.copyTo(final(Rect(0, 0, rez.cols, rez.rows)));
-	legenda.copyTo(final(Rect(rez.cols,0, legenda.cols, legenda.rows)));
-	*/
 	Mat3b imagine_finala = ataseazaLegenda(rez, 280);
 
 	imshow("FINAL", imagine_finala);
